72e17360ad97c2ff843a007bd6602f554da69d8d
Fathy Hosny <ali01120939266@gmail.com>
2026-05-19 18:26:16 +0300

    Add project files.

---
+++ GymManagement/Controllers/PlansController.cs
@@ -0,0 +1,29 @@
+﻿using GymManagement.DbContexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManagement.Controllers
+{
+    public class PlansController : Controller
+    {
+        public readonly GymDbcontext _context = new GymDbcontext();
+        public async Task<IActionResult> Index()
+        {
+            var plans = await _context.Plans.ToListAsync();
+            return View(plans);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var plan = _context.Plans.FirstOrDefault(p => p.Id == id);
+            if (plan is null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            return View(plan);
+        }
+
+
+
+    }
+}
